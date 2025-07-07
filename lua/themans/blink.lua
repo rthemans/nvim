edbdcb10d72eff52093bdc02d5933fdef8fb9150
Raphaël Thémans <raph_themans09@msn.com>
2025-07-07 17:19:24 +0200

    Create blink.lua

---
+++ lua/themans/blink.lua
@@ -0,0 +1,32 @@
+require("blink.cmp").setup({
+    completion = {
+            menu = {
+                draw = {
+                    -- We don't need label_description now because label and label_description are already
+                    -- combined together in label by colorful-menu.nvim.
+                    columns = { { "kind_icon" }, { "label", gap = 1 } },
+                    components = {
+                        label = {
+                            text = function(ctx)
+                                return require("colorful-menu").blink_components_text(ctx)
+                            end,
+                            highlight = function(ctx)
+                                return require("colorful-menu").blink_components_highlight(ctx)
+                            end,
+                        },
+                    },
+                },
+            },
+        },
+    keymap = {
+        preset = "default",
+        ["<tab>"] = { "select_and_accept" , "fallback"},
+    },
+    fuzzy = {
+        implementation = "prefer_rust_with_warning", 
+        prebuilt_binaries = {
+            download = false,
+            ignore_version_mismatch = true,
+        }
+    },
+})
